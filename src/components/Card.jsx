e427c1401b9654e55a3a7bfe04a1b6a7cbff0f41
juanoned <JuanDiego.jd42@gmail.com>
2024-09-05 17:17:57 -0300

    Make Feat 2.5

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable react/prop-types */
+import style from '../App.module.css';
+
 export const Card = (props) => {
   return(
-      <div>
+      <div className={style.card}>
           <h1>{props.name}</h1>
           <h2>{props.desc}</h2>
           <p>{props.value}</p>
+          <div className={`${props.status}?'greenball':'redball'`}></div>
           <img src={props.image} alt={props.name} width={150} height={"auto"}/>
       </div>
   )
