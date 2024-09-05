94f19ab93a75de9c4bcf337595ccad1b80af4879
juanoned <JuanDiego.jd42@gmail.com>
2024-09-05 17:24:23 -0300

    Make Feat 2.5

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ export const Card = (props) => {
           <h1>{props.name}</h1>
           <h2>{props.desc}</h2>
           <p>{props.value}</p>
-          <div className={(props.status?"greenball":"redball")}></div>
+          <div className={(props.status?"greenBall":"redBall")}></div>
           <img src={props.image} alt={props.name} width={150} height={"auto"}/>
       </div>
   )
