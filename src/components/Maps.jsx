214365ef2aec5a92185174b37bcb4f0c4a380ad6
juanoned <juanDiego.jd42@gmail.com>
2024-09-06 15:41:23 -0300

    MAKE MAP

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -6,7 +6,7 @@ import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 export const Maps = () => {
     const position = [-25.4249943, -49.2722531]
     return(
-        <MapContainer center={position} zoom={15} scrollWheelZoom={false} style={{width: "100%", height: "700px"}}>
+        <MapContainer center={position} zoom={15} scrollWheelZoom={true} style={{width: "100%", height: "700px"}}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
