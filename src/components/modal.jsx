25fd7273d2e21861a6043799709c654f1e2deaf0
JuanOneD <juandiego.jd42@gmail.com>
2024-09-12 16:36:19 -0300

    Botao de fechar modal

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,11 +1,13 @@
 import Draggable from 'react-draggable';
 import { Card2 } from '../../src/components/Card2'
 
+
 const ModalInfo = (props) => {
     return(
         <Draggable>
             <div style={{position:'absolute',zIndex:'22'}}>
-            <Card2 name={props.data.name} status={props.data.status} species={props.data.species} type={props.data.type} gender ={props.data.gender} image={props.data.image}/>   
+            <Card2 name={props.data.name} status={props.data.status} species={props.data.species} type={props.data.type} gender ={props.data.gender} image={props.data.image}/>
+            <button onClick={props.close} style={{width:'100%'}}>&#128473;</button> 
             </div>
         </Draggable>
     )
