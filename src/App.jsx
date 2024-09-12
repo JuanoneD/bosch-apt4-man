cca4455e4a4a657ec97eb5ffbc3d79e6ca7e123e
JuanOneD <juandiego.jd42@gmail.com>
2024-09-12 17:06:54 -0300

    Estilização 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ function App() {
   return (
     <>
     <div className={style.wrapBtns}>
-      <a href='/Products'><button>Produtos</button></a>
-      <a href='/API'><button>API</button></a>
-      <a href='/Map'><button>Mapa</button></a>
-      <a href='/Graph'><button>Grafico</button></a>
+      <a href='/Products'><button style={{borderRadius:'10px'}}>Produtos</button></a>
+      <a href='/API'><button style={{borderRadius:'10px'}}>API</button></a>
+      <a href='/Map'><button style={{borderRadius:'10px'}}>Mapa</button></a>
+      <a href='/Graph'><button style={{borderRadius:'10px'}} >Grafico</button></a>
     </div>
     </>
   )
